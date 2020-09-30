fd8437cf6abc71f2a4b2b481b18bd290128e8144
raul.24672@gmail.com <raul.24672@gmail.com>
2020-09-30 20:54:45 -0300

    agregué método solicitado reqMap02

--- src/main/java/com/tpa/git/MainController.java
+++ src/main/java/com/tpa/git/MainController.java
@@ -1,5 +1,6 @@
 package com.tpa.git;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ public class MainController {
     @RequestMapping(path = "/metodo1")
     public String reqMap01(){
         return "metodo1";
+    }
+
+    @RequestMapping(path = "/metodo2/{parametro1}")
+    public String reqMap02(@PathVariable String parametro1){
+        return parametro1;
     }
 
 }
